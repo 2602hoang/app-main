3a3f11e958f8bd5dd0d6cbef3b510168d68791f2
2602hoang <vuhuyhoangboj@gmail.com>
2025-04-25 17:07:59 +0700

    fix

--- src/components/ProfileData.tsx
+++ src/components/ProfileData.tsx
@@ -1,36 +1,28 @@
-import { useToken } from '@/hooks/useToken';
-import { useUserStore } from '@/stores/userStore';
-import axios from 'axios';
-import React from 'react'
-
 const ProfileData = () => {
-
-    const { accessToken } = useToken();
-    const { setUser } = useUserStore();
-    const getUserInfo = async () => {
-        try {
-            const response = await axios.get(
-                "https://p2p-gateway-sandbox.up.railway.app/api/v1/users/profile",
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                }
-            );
-            if (response.status === 200) {
-                setUser(response.data.data);
-            } else {
-                console.error('Failed to fetch user info:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error fetching user info:', error);
-        }
-    };
-    React.useEffect(() => {
-        if (accessToken) {
-            getUserInfo();
-        }
-    }, [accessToken]);
+    // const getUserInfo = async () => {
+    //     try {
+    //         const response = await axios.get(
+    //             "https://p2p-gateway-sandbox.up.railway.app/api/v1/users/profile",
+    //             {
+    //                 headers: {
+    //                     Authorization: `Bearer ${accessToken}`,
+    //                 },
+    //             }
+    //         );
+    //         if (response.status === 200) {
+    //             setUser(response.data.data);
+    //         } else {
+    //             console.error('Failed to fetch user info:', response.statusText);
+    //         }
+    //     } catch (error) {
+    //         console.error('Error fetching user info:', error);
+    //     }
+    // };
+    // React.useEffect(() => {
+    //     if (accessToken) {
+    //         getUserInfo();
+    //     }
+    // }, [accessToken]);
     
     return null;
 }
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,11 @@
-// lib/redis.ts
-import Redis from "ioredis";
+// // lib/redis.ts
+// import Redis from "ioredis";
 
-// Redis mặc định localhost:6379
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  username: process.env.REDIS_USER,
-  //   password: process.env.REDIS_PASSWORD,
-});
-export default redis;
+// // Redis mặc định localhost:6379
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || "127.0.0.1",
+//   port: Number(process.env.REDIS_PORT) || 6379,
+//   username: process.env.REDIS_USER,
+//   //   password: process.env.REDIS_PASSWORD,
+// });
+// export default redis;
