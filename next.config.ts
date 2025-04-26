3dd6a6aba037cf3c17cc5fddd513315ac046dd10
2602hoang <vuhuyhoangboj@gmail.com>
2025-04-26 10:47:35 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,11 @@ const nextConfig = {
       },
       {
         source: "/app-1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_APP_1}/app-1/app-1/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_APP_1}/:path*`,
       },
       {
         source: "/app-2/:path*",
-        destination: `${process.env.NEXT_PUBLIC_APP_2}/app-2/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_APP_2}/:path*`,
       },
       {
         source: "/",
@@ -30,7 +30,7 @@ const nextConfig = {
       },
       {
         source: "/",
-        destination: `${process.env.NEXT_PUBLIC_MAIN}`,
+        destination: `${process.env.NEXT_PUBLIC_APP_2}`,
         has: [
           {
             type: "header",
