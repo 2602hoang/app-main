4f1b3a408bb671c2e19d490ba7d05113c18cd2e4
2602hoang <vuhuyhoangboj@gmail.com>
2025-04-26 14:19:54 +0700

    fix main

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,11 @@ const nextConfig = {
       },
       {
         source: `/${process.env.NEXT_NAME_APP1}/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_APP_1}/${process.env.NEXT_NAME_APP1}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_APP_1}/:path*`,
       },
       {
         source: `/${process.env.NEXT_NAME_APP2}/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_APP_2}/${process.env.NEXT_NAME_APP2}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_APP_2}/:path*`,
       },
       {
         source: "/",
@@ -44,3 +44,15 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// NEXT_PUBLIC_APP1_INIT_KEY=user-storage-app1
+
+// # NEXT_PUBLIC_APP_1=https://app-1-rho.vercel.app
+// NEXT_NAME_APP1=app-1
+// NEXT_NAME_APP2=app-2
+
+// # NEXT_PUBLIC_APP_2=http://localhost:4001
+// NEXT_PUBLIC_APP_2=https://app-2-inky.vercel.app
+// NEXT_PUBLIC_MAIN=http://localhost:3000/
+
+// NEXTAUTH_SECRET=some_super_secret_value
