491f14dcceda2769e8bdee974ce252f31e254357
2602hoang <vuhuyhoangboj@gmail.com>
2025-04-26 09:30:32 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -12,10 +12,12 @@ const nextConfig = {
       {
         source: "/app-1/:path*",
         destination: `${process.env.NEXT_PUBLIC_APP_1}/app-1/:path*`,
+        permanent: true,
       },
       {
         source: "/app-2/:path*",
         destination: `${process.env.NEXT_PUBLIC_APP_2}/app-2/:path*`,
+        permanent: true,
       },
       {
         source: "/",
