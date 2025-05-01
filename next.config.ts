a72ee5a55ddb92b6d1d8eac9a5d1f33be1662187
2602hoang <vuhuyhoangboj@gmail.com>
2025-05-01 15:44:48 +0700

    fix app zone roi add middlewear

--- next.config.ts
+++ next.config.ts
@@ -17,28 +17,28 @@ const nextConfig = {
         source: `/app-2/:path*`,
         destination: `https://app-2-test.vercel.app/app-2/:path*`,
       },
-      {
-        source: "/",
-        destination: `https://app-main-test.vercel.app/`,
-        has: [
-          {
-            type: "header",
-            key: "referer",
-            value: `https://app-1-test.vercel.app`,
-          },
-        ],
-      },
-      {
-        source: "/",
-        destination: `https://app-main-test.vercel.app/`,
-        has: [
-          {
-            type: "header",
-            key: "referer",
-            value: `https://app-2-test.vercel.app`,
-          },
-        ],
-      },
+      // {
+      //   source: "/",
+      //   destination: `https://app-main-test.vercel.app/`,
+      //   has: [
+      //     {
+      //       type: "header",
+      //       key: "referer",
+      //       value: `https://app-1-test.vercel.app/app-1`,
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/",
+      //   destination: `https://app-main-test.vercel.app/`,
+      //   has: [
+      //     {
+      //       type: "header",
+      //       key: "referer",
+      //       value: `https://app-2-test.vercel.app/app-2`,
+      //     },
+      //   ],
+      // },
     ];
   },
 };
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const referer = request.headers.get("referer");
+
+  if (referer?.includes("app-1-test.vercel.app/app-1")) {
+    return NextResponse.redirect("https://app-main-test.vercel.app/");
+  }
+
+  if (referer?.includes("app-2-test.vercel.app/app-2")) {
+    return NextResponse.redirect("https://app-main-test.vercel.app/");
+  }
+
+  return NextResponse.next();
+}
