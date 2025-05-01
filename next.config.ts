c3677d1a2dcc899724660ee095c4bfe4538a5b65
2602hoang <vuhuyhoangboj@gmail.com>
2025-05-01 16:24:07 +0700

    fix end

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: "/:path*",
-        destination: "/:path*",
-      },
+      // {
+      //   source: "/:path*",
+      //   destination: "/:path*",
+      // },
       {
         source: `/app-1/:path*`,
         destination: `https://app-1-test.vercel.app/app-1/:path*`,
@@ -17,28 +17,28 @@ const nextConfig = {
         source: `/app-2/:path*`,
         destination: `https://app-2-test.vercel.app/app-2/:path*`,
       },
-      // {
-      //   source: "/",
-      //   destination: `https://app-main-test.vercel.app/`,
-      //   has: [
-      //     {
-      //       type: "header",
-      //       key: "referer",
-      //       value: `https://app-1-test.vercel.app/app-1`,
-      //     },
-      //   ],
-      // },
-      // {
-      //   source: "/",
-      //   destination: `https://app-main-test.vercel.app/`,
-      //   has: [
-      //     {
-      //       type: "header",
-      //       key: "referer",
-      //       value: `https://app-2-test.vercel.app/app-2`,
-      //     },
-      //   ],
-      // },
+      {
+        source: "/",
+        destination: `https://app-main-test.vercel.app/`,
+        has: [
+          {
+            type: "header",
+            key: "referer",
+            value: `https://app-1-test.vercel.app/app-1`,
+          },
+        ],
+      },
+      {
+        source: "/",
+        destination: `https://app-main-test.vercel.app/app-2`,
+        has: [
+          {
+            type: "header",
+            key: "referer",
+            value: `https://app-2-test.vercel.app/app-2`,
+          },
+        ],
+      },
     ];
   },
 };
